1fc5d259d857da7a958817cc415a0eb30faf8bab
HyunShik <hshong0907@gmail.com>
2023-11-09 17:04:28 +0900

    chore: Jwt 관련 상수 저장

---
+++ src/main/java/org/example/common/constants/JwtConstants.java
@@ -0,0 +1,7 @@
+package org.example.common.constants;
+
+public final class JwtConstants {
+    public static final String HEADER = "Authorization";
+    public static final String TYPE = "Bearer";
+}
+
