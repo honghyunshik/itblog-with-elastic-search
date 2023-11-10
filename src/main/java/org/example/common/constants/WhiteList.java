4c294f548df368304fee03ab737f104146bda411
HyunShik <hshong0907@gmail.com>
2023-11-10 17:58:55 +0900

    docs: WhiteList에 login url 추가

--- src/main/java/org/example/common/constants/WhiteList.java
+++ src/main/java/org/example/common/constants/WhiteList.java
@@ -2,6 +2,8 @@ package org.example.common.constants;
 
 public class WhiteList {
     public static final String[] WHITE_LIST = {
-            "/api/v1/member/**"
+            "/api/v1/member/**",
+            "/login",
+            "/"
     };
 }
