42a70093f74464fedee4662501950b43b60dea76
hyunshik <hshong0907@gmail.com>
2024-01-02 22:26:40 +0900

    chore: Login Entity에 Getter 메서드 추가

--- src/main/java/org/example/domain/login/Login.java
+++ src/main/java/org/example/domain/login/Login.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class Login {
 
     @Id
