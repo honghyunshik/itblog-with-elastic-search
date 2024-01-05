57f5074330b4ba7d6e95dfd36412b5d1279cfbe3
hyunshik <hshong0907@gmail.com>
2024-01-05 23:45:40 +0900

    refactor: redis 삭제

--- src/main/java/org/example/config/JwtTokenProvider.java
+++ src/main/java/org/example/config/JwtTokenProvider.java
@@ -7,10 +7,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.example.common.constants.JwtConstants;
+import org.example.common.constants.JwtExpiration;
 import org.example.dto.login.TokenInfo;
 import org.example.service.impl.MemberLoginServiceimpl;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -20,6 +20,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -27,46 +29,22 @@ import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenProvider {
-
-    //private RedisLoginServiceImpl redisLoginService;
-
-    private MemberLoginServiceimpl memberLoginServiceimpl
     private Key key;
 
-    //기간 30분
-    private static int ACCESS_EXPIRE_TIME = 30*60*1000;
-    //기간 1주일
-    private static int REFRESH_EXPIRE_TIME = 7*24*60*60*1000;
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
 
-    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey, MemberLoginServiceimpl memberLoginServiceimpl) {
-
-        this.memberLoginServiceimpl = memberLoginServiceimpl;
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    //Access Token이 만료됐지만 refresh Token이 Redis에 존재할 경우 새로운 access Token 발급
+    //Access Token이 만료됐지만 refresh Token이 유효할 경우 새로운 access Token 발급
     //발급하는 기간은 30분과 refresh Token의 min값
-    public TokenInfo refreshAccessToken(Authentication authentication){
+    public String refreshAccessToken(String refreshToken){
 
-        LoginRedisResponseDto loginRedisResponseDto = redisLoginService.tokenExist(authentication.getName());
-        if(loginRedisResponseDto==null) return null;
-        String refreshToken = loginRedisResponseDto.getToken();
-        int refreshTTL = loginRedisResponseDto.getExpiration();
-        String accessToken = generateAccessToken(authentication,generateAuthorities(authentication),
-                Math.min(ACCESS_EXPIRE_TIME,refreshTTL));
-        return TokenInfo.builder()
-                .grantType(JwtConstants.TYPE)
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
+        Authentication authentication = getAuthentication(refreshToken);
 
-    //토큰의 만료기간 얻기
-    public int getExpirationTime(String token){
-        Date expiration = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getExpiration();
-        long now = new Date().getTime();
-        return (int) (expiration.getTime()-now);
+        return generateAccessToken(authentication,
+        generateAuthorities(authentication), JwtExpiration.ACCESS_TOKEN_EXPIRATION);
     }
 
     public String generateAuthorities(Authentication authentication){
@@ -78,7 +56,7 @@ public class JwtTokenProvider {
 
     //accessToken의 유지기간은 30분
     //refreshToken이 30분 이하로 남았을 경우 refresh Token이 만료된 이후 로그인 필요
-    public String generateAccessToken(Authentication authentication, String authorities, int time){
+    public String generateAccessToken(Authentication authentication, String authorities, long time){
 
         long now = new Date().getTime();
 
@@ -98,7 +76,7 @@ public class JwtTokenProvider {
         return Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim("auth",authorities)
-                .setExpiration(new Date(now + REFRESH_EXPIRE_TIME))
+                .setExpiration(new Date(now + JwtExpiration.REFRESH_TOKEN_EXPIRATION))
                 .signWith(key,SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -108,17 +86,10 @@ public class JwtTokenProvider {
         String authorities = generateAuthorities(authentication);
 
         //accessToken 유지기간은 30분
-        String accessToken = generateAccessToken(authentication,authorities,ACCESS_EXPIRE_TIME);
+        String accessToken = generateAccessToken(authentication,authorities,JwtExpiration.ACCESS_TOKEN_EXPIRATION);
 
         //refreshToken 유지기간은 2주
         String refreshToken = generateRefreshToken(authentication,authorities);
-
-        //refresh token redis 저장
-        redisLoginService.save(LoginRedisRequestDto.builder()
-                        .email(authentication.getName())
-                        .token(refreshToken)
-                        .expiration(REFRESH_EXPIRE_TIME)
-                        .build());
 
         return TokenInfo.builder()
                 .grantType(JwtConstants.TYPE)
@@ -141,9 +112,9 @@ public class JwtTokenProvider {
         Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
     }
 
-    public Authentication getAuthentication(String accessToken){
+    public Authentication getAuthentication(String token){
 
-        Claims claims = parseClaims(accessToken);
+        Claims claims = parseClaims(token);
 
         if(claims.get("auth")==null) throw new RuntimeException("권한 정보가 없는 토큰입니다");
 
