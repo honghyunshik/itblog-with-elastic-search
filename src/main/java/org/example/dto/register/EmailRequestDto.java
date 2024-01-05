0bf66a60d50903ac1ff357061acb8c7eeee35a5d
hyunshik <hshong0907@gmail.com>
2024-01-05 23:35:42 +0900

    chore: 요청/응답 Dto 생성

--- src/main/java/org/example/dto/register/EmailRequestDto.java
+++ src/main/java/org/example/dto/register/EmailRequestDto.java
@@ -6,13 +6,11 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmailRequestDto{
+@Getter
+public class EmailRequestDto {
     @NotNull(message = "이메일은 필수 입력사항입니다")
     @NotBlank
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$")
