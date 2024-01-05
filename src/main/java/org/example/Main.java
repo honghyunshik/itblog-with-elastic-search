33c4089ffefc332c64d918ce85f1e99efa5072a4
hyunshik <hshong0907@gmail.com>
2024-01-05 23:42:45 +0900

    refactor: redis 의존성 삭제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
