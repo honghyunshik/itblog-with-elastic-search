c4fe8fdb0fda0d66266f09fb49dd3af4ea92f347
hyunshik <hshong0907@gmail.com>
2024-01-08 18:41:05 +0900

    docs: whitelist에 도메인 변경된거 적용

--- src/main/java/org/example/common/constants/WhiteList.java
+++ src/main/java/org/example/common/constants/WhiteList.java
@@ -3,12 +3,14 @@ package org.example.common.constants;
 public class WhiteList {
     public static final String[] WHITE_LIST = {
             "/api/v1/member/**",
-            "/login",
             "/css/**",
             "/",
             "/js/**",
             "/favicon.ico",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/itblog/login",
+            "/itblog/register",
+            "/itblog/index"
     };
 }
